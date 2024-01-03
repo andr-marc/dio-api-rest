1070e8ce02922fa6569f00e562c26979ecc1fd8b
André <andre.marcelino@parnamirim.rn.gov.br>
2024-01-03 08:15:26 -0300

    first commit

---
+++ src/main/java/com/andremarc/diorestapi/DioRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.andremarc.diorestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andremarc/diorestapi/DioRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andremarc.diorestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
